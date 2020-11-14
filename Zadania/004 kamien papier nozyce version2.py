92b14bd2e04422d748f51d4ebcaaf2ec02343fce
Krzysztof Wisniewski <cychug@gmail.com>
2020-11-14 18:46:32 +0100

    new version of kamien nozyce papier

--- Zadania/004 kamien papier nozyce version2.py
+++ Zadania/004 kamien papier nozyce version2.py
@@ -1,12 +1,11 @@
-print("Gra w papier, kamień, nozyczki. c - kończy grę")
-u = "p"
-while u != "c":
-    u = input("Uzytkownik podaj p - papier, k - kamień lub n - nozyczki.")
+koniec = 0
+while koniec == 0:
+    u = input("Uzytkownik podaj p - papier, k - kamień lub n - nozyczki. Inny kończy grę. ")
     if (u == "p") or (u == "k") or (u == "n"):
-        m = input("Macbook podaj p - papier, k - kamień lub n - nozyczki.")
+        m = input("Macbook podaj p - papier, k - kamień lub n - nozyczki. Inny kończy grę. ")
         if (m == "p") or (m == "k") or (m == "n"):
             if (u == "p") and (m == "p"):
-                print("Remis")
+                print("remis")
             elif (u == "p") and (m == "k"):
                 print("u wygrał")
             elif (u == "p") and (m == "n"):
@@ -23,4 +22,8 @@ while u != "c":
                 print("m wygrał")
             elif (u == "n") and (m == "n"):
                 print("remis")
+        else:
+            koniec = 1
+    else:
+        koniec = 1
 print("Koniec gry")
