85ce70c488ea9672f37382245b5373cd63452c97
Krzysztof Wisniewski <cychug@gmail.com>
2020-11-16 17:13:31 +0100

    sets function and operation

--- 035 sets zbiory operacje i funkcje.py
+++ 035 sets zbiory operacje i funkcje.py
@@ -7,13 +7,23 @@
 # listy         imiona = ["Krzysiek", "Ania", "Bartek", "Kuba", "Robert"]
 # krotki        krotka = (1, 42, 12, -4)
 # słowniki      pokoje = {49: 'Krzysztof Abacki', 69: 'Marek Bebacki'}
-# zbiory        A = {1, 4, 20, -4, 20}  BONUS: | (suma zbiorów) & (wspólny) - ^
+# zbiory        A = {1, 4, 20, -4, 20}
+# # BONUS: | (suma zbiorów), & (wspólne), - (odemjmowanie od A odejmi B), ^ - ksor (wyklucza wspólne wartości)
+
+# A.add(1)       dodanie 1
+# A.discard(7)   usunięcie 7
+# A.remove(24)   usunięcie 24 ale jak nie będzie to zwróci błąd !!!
+# A.subset(b)   czy zbiór A jest podzbiorem B?
 
 A = {1, 4, 20, -4, 20, 400}          # elementy powinny być unikalne NIE ZOSTANIE WYŚWIETLONY 20 DWA RAZY
 print("Oto zbiór ", A)
 
 A.add(7)                        # dodanie elementu
 print("Dodałem element 7 ", A)
+A.discard(7)
+print("usunąłem 7: ", A)
+D = {1, 4}
+print(D.issubset(A))            # czy D jest podzbiorem A
 
 B = [1, 4, 20, -4, 20]          # TO JEST LISTA!!!
 print("To jest lista B: ", B)
@@ -26,3 +36,6 @@ print("Zbiór C ", C)
 print("Wspólne elementy A i B i C: ", A & B & C)    # wspólne elementy zbiorów (baz)
 print("Suma zbiorów A i B: ", A | B)                # suma zbiorów
 print("Odejmowanie zioru A - C", A - C)             # odejmowanie zbiorów
+print("Zbiór A: ", A)
+print("Zbiór B: ", B)
+print("^ wyklucza wspólne wartości: ", A ^ B)       # wypisuje wszystkie oprócz wspólnych wartości (ksor)
