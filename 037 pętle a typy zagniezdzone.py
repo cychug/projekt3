2510e0279455cb2391c2cf5287afe18e0111bf51
Krzysztof Wisniewski <cychug@gmail.com>
2020-11-18 19:46:21 +0100

    operacje na zbiorach zagniezdzonych

---
+++ 037 pętle a typy zagniezdzone.py
@@ -0,0 +1,12 @@
+# operacje na listach zagniezdzonych
+
+zbiorGosci1 = {('Krzysiek', 45, 'mezczyzna', 9876543),
+               ('Ania', 46, 'kobieta', 82344553),
+               ('Hania', 15, 'kobieta', 4567890)}
+
+for imie, wiek, plec, numerTelefonu in zbiorGosci1:
+    print("Imię: ", imie)
+    print("Wiek: ", wiek)
+    print("Płeć: ", plec)
+    print("Numer teleonu: ", numerTelefonu)
+    print("\n")             # po kazdym print() jest dodany koniec linii, \n zrobi dwa
