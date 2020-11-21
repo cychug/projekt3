8397b7d96d49e7c09d2d0cab654fba00d79918f9
Krzysztof Wisniewski <cychug@gmail.com>
2020-11-21 18:02:57 +0100

    szybszy sposób

--- 041 Wypisywanie wartosci słownika zagniezdzonego w liscie i slowniku.py
+++ 041 Wypisywanie wartosci słownika zagniezdzonego w liscie i slowniku.py
@@ -36,17 +36,27 @@ people2 = [
 for record in people2:              # dla kadego rekordu (który jest słownikiem) w liscie peopple2 wykonaj pętlę poniej
     for key in record:              # dla kadego klucza w rekordzie
         print(key, record[key])     # wypisz nazwę klucza oraz jego wartość
-       
+
 # WYŚWIETLENIE WARTOŚCI SŁOWNIKAÓW ZE ZBIORU SŁOWNIKÓW
 people1 = {
             'id1': {'name': 'Krzysiek', 'age': 45, 'sex': 'male'},
             'id2': {'name': 'Ania', 'age': 50, 'sex': 'female'}
           }
-print("To jest wyświetlenie całego słownika: ", people1)
-print()
-print("To jest wyświetlenie konkretnego elementu słownika: ", people1['id2'])
-print()
-
 for key in people1:
-   for secondaryKey in people1[key]:
-       print(secondaryKey, people1[key][secondaryKey])
+    print(key)
+    for secondaryKey in people1[key]:
+        print(secondaryKey, people1[key][secondaryKey])
+
+# WYŚWIETLENIE WARTOŚCI SŁOWNIKÓW ZE ZBIORU - LEPSZE I SZYBSZE (WG. PRAKTYWÓW
+print("######### lepszy sposób #########")
+people1 = {
+            'id1': {'name': 'Krzysiek', 'age': 45, 'sex': 'male'},
+            'id2': {'name': 'Ania', 'age': 50, 'sex': 'female'}
+          }
+for id, dictionary in people1.items():      # zastosowanie .items()
+    print("ID", id)
+    for key in dictionary:
+        print(key, ":", dictionary[key])
+
+
+print(people1.items())          # wyświetli całą zawartość
