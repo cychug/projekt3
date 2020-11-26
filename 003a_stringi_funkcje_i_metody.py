00a04f701db8d1eee2db0dfc6412d44604e240a2
Krzysztof Wisniewski <cychug@gmail.com>
2020-11-26 18:56:23 +0100

    ola

--- 003a_stringi_funkcje_i_metody.py
+++ 003a_stringi_funkcje_i_metody.py
@@ -5,11 +5,26 @@ print(len(name))    # funkcja
 # METODY
 print(name.capitalize())    # pierwsza litera bedzie zmieniona na wielka
 print(name.upper())         # wszystkie litery zmienione na wielkie
-print(name1.lower())         # wszysktie na małe
+print(name1.lower())        # wszysktie na małe
 
 print(name[0])              # pierwsza litera
 print(name[-1])             # ostatnia litera
 print(name[0:2])            # pierwsze dwie lietry
 print(name[3:])             # od trzeciej włącznie litery do KOŃCA
 print(name[:-3])            # od początku do -3
-print(name[-3:])           # od -3 do do końca
+print(name[-3:])            # od -3 do do końca
+
+channel = "Jak napisać ksiązkę"
+print(channel.split(" "))   # utworzy listę z wyrazami podzielonymi spacją
+
+join_string = " "           # czym połączymy, w tym przypadku spacją
+print(join_string.join(['Jak', 'napisać', 'ksiązkę.']))     # metoda .join
+print(name.startswith("K"))    # sprawdzenie czy string zaczyna się literą, wielkość ma znaczenie
+print(name.startswith("k"))
+
+print(name.endswith("k"))       # sprawdzenie czy string kończy się literą, wielkość ma znaczenie
+
+print(name.rstrip("k"))         # usunięcie z prawej strony litery k (tylko na brzegach)
+print(name.lstrip("k"))         # usunięcie z lewej strony litery k (tylko na brzegach)
+
+print(name.strip("k"))         # usunięcie z prawej i lewej strony litery k (tylko na brzegach)
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # projekt3
-# tu są moje programy i dokumentacja do nauki python'a
+tu są moje programy i dokumentacja do nauki python'as
+
+## Rozdział 1 u jest akapit
+* Podrozdział A
+	* asdfasdf
+	* asdfasdf
+* Podrozdział B
+
+## Rozdział 2
+
+
+# Rozdział 3
